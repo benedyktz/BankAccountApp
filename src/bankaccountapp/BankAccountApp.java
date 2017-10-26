02e0133b9162aa9fea00d1600df941ed4cc68c61
Benedykt <ziolkowski.benedykt@gmail.com>
2017-10-26 18:44:52 +0200

    add CsvReader class

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -4,24 +4,27 @@ public class BankAccountApp {
 
 	public static void main(String[] args) {
 		
-		CheckingAccount checkingAccount1 = new CheckingAccount("aa bb", "12345678", 1500);
-		
-		SavingsAccount savingsAccount1 = new SavingsAccount("cc dd", "87654321", 2000);
-		
-		savingsAccount1.showInfo();
-		System.out.println("***************");
-		checkingAccount1.showInfo();
-		
-		savingsAccount1.deposit(100);
-		savingsAccount1.withdraw(200);
-		savingsAccount1.transfer("aa", 12);
-		
-		checkingAccount1.deposit(10);
-		checkingAccount1.withdraw(100);
-		checkingAccount1.transfer("adasd", 20);
 		
 		
-		savingsAccount1.compound();
+		
+//		CheckingAccount checkingAccount1 = new CheckingAccount("aa bb", "12345678", 1500);
+//		
+//		SavingsAccount savingsAccount1 = new SavingsAccount("cc dd", "87654321", 2000);
+//		
+//		savingsAccount1.showInfo();
+//		System.out.println("***************");
+//		checkingAccount1.showInfo();
+//		
+//		savingsAccount1.deposit(100);
+//		savingsAccount1.withdraw(200);
+//		savingsAccount1.transfer("aa", 12);
+//		
+//		checkingAccount1.deposit(10);
+//		checkingAccount1.withdraw(100);
+//		checkingAccount1.transfer("adasd", 20);
+//		
+//		
+//		savingsAccount1.compound();
 
 	}
 
