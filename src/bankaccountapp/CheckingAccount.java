a713ac9912410121a79516286f0a0e5f5cd5707d
Benedykt <ziolkowski.benedykt@gmail.com>
2017-10-26 16:37:05 +0200

    set debit card in the constructor of checking account class

--- src/bankaccountapp/CheckingAccount.java
+++ src/bankaccountapp/CheckingAccount.java
@@ -3,21 +3,24 @@ package bankaccountapp;
 public class CheckingAccount extends Account {
 	
 	//list properties specific to checking accounts
-	private int debitCardNumber;
-	private int debitCardPin;
+	private final int debitCardNumber;
+	private final int debitCardPin;
 	
 	//constructor to initialize checking accounts properties
 	public CheckingAccount(String name, String socialSecurityNumber, double initDeposit) {
 		super(name, socialSecurityNumber, initDeposit);
 		accountNumber = "2" + accountNumber;
-		
+		debitCardNumber = (int) (Math.random() * Math.pow(10, 12));
+		debitCardPin = (int) (Math.random() * Math.pow(10, 4));
 	}
 	
 	//list methods specific to checking accounts
-	
+
 	public void showInfo() {
 		System.out.println("checking");
 		super.showInfo();
+		System.out.println("Debit Card Number: " + debitCardNumber + 
+				"\nDebit Card PIN: " + debitCardPin);
 	}
 
 }
--- src/bankaccountapp/SavingsAccount.java
+++ src/bankaccountapp/SavingsAccount.java
@@ -5,22 +5,19 @@ import javax.print.attribute.SetOfIntegerSyntax;
 public class SavingsAccount extends Account {
 	
 	//list properties specific to savings accounts
-	private int safetyDepositBoxId;
-	private int safetyDepositBoxKey;
+	private final int safetyDepositBoxId;
+	private final int safetyDepositBoxKey;
 	
 	//constructor to initialize saving accounts properties
 	public SavingsAccount(String name, String socialSecurityNumber, double initDeposit) {
 		super(name, socialSecurityNumber, initDeposit);
 		accountNumber = "1" + accountNumber;
-		setSafetyDepositBox();
-	}
-	
-	//list methods specific to savings accounts
-	
-	private void setSafetyDepositBox() {
 		safetyDepositBoxId = (int) (Math.random() * 1000);
 		safetyDepositBoxKey = (int) (Math.random() * 10000);
 	}
+	
+	//list methods specific to savings accounts
+
 	
 	public void showInfo() {
 		System.out.println("savings");
